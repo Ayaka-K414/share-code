135734902e74d47b5771074d278a938ecee3d4d6
ayakakobayashi <414ayaka@gmail.com>
2024-06-30 23:48:14 +0900

    画面幅768px以上のレスポンシブまで

---
+++ assets/post-BO2E499C.js
@@ -0,0 +1,118 @@
+import"./ress-B710VfZE.js";import{l as s,i as a,h as l}from"./hamburger-button-CdvTyI2V.js";const i="/assets/post2304_01-U1FAFb5v.jpg",t="/assets/post2304_02-DgImFewq.jpg",e="/assets/post2304_03-_VMcRd9X.jpg",r="/assets/post2304_04-Dqau3a_q.jpg",c="/assets/post2304_05--5bTuhMg.jpg",o="/assets/post2304_06-CEN_0j1e.jpg",p="/assets/post2304_07-Cle8iTkU.jpg";document.querySelector("#app").innerHTML=`
+    <div>
+        <header>
+        <a href="#"><img class="logo" src=${s} alt="GTBサービスのロゴ" /></a>
+        <nav>
+            <li><a href="">私たちについて</a></li>
+            <li><a href="">事業案内</a></li>
+            <li><a href="">アクセス</a></li>
+            <li><a href="">施工実績</a></li>
+            <li class="list-contact"><a href="">お問い合わせ</a></li>
+            <li class="insta"><a href=""><img src=${a} art="Instagramアイコン"></a></li>
+        </nav>
+        <button class="hamburger">
+            <img src="${l}" alt="ハンバーアイコン" />
+        </button>
+        </header>
+        <main>
+            <article>
+                <div class="title">
+                    <div class="category">
+                        <h4 class="category-title">Works</h4>
+                        <p class="category-subtitle">・施工実績・</p>
+                    </div>
+                    <span class="year-month">2023.4</span>
+                    <span class="tag">レストア</span>
+                    <p class="title-text">ホンダN360のレストア</p>
+                </div>
+                <div class="img-container">
+                    <div class="before img-contents">
+                        <p class="vertical-title">before</p>
+                        <div class="before-img">
+                            <img src=${i} alt="ホンダN360のレストア" />
+                            <img src=${t} alt="ホンダN360のレストア" />
+                        </div>
+                    </div>
+                    <div class="process img-contents">
+                        <p class="vertical-title">process</p>
+                        <div class="process-img">
+                            <img src=${e} alt="ホンダN360のレストア" />
+                            <img src=${r} alt="ホンダN360のレストア" />
+                            <img src=${c} alt="ホンダN360のレストア" />
+                        </div>
+                    </div>
+                    <div class="after img-contents">
+                        <p class="vertical-title">after</p>
+                        <div class="after-img">
+                            <img src=${o} alt="ホンダN360のレストア" />
+                            <img src=${p} alt="ホンダN360のレストア" />
+                        </div>
+                    </div>
+                </div>
+                <div class="text-container">
+                    <p>（例文）<br>1970年式のホンダN360。<br>素敵な旧車のレストア作業です。</p>
+                    <p>ボディは劣化した塗装をすべて剥がして下処理、腐食して変形してしまっている部分をパテで成形しました。<br>外装、内装パーツを解体して清掃します。ボディは劣化した塗装をすべて剥がして下処理、腐食して変形してしまっている部分をパテで成形しました。</p>
+                    <p>全塗装のカラーは旧車らしい深みのあるレッド、天井はブラックにしツートンカラーに仕上げました。</p>
+                    <p>綺麗になった愛車と今後も楽しい時間をお過ごしください！</p>
+                </div>
+                <div class="pagenation">
+                    <a class="prev" href="">前の記事へ</a>
+                    <a class="list" href="">一覧へ戻る</a>
+                    <a class="next" href="">次のページ</a>
+                </div>
+            </article>
+        </main>
+        <footer>
+            <div class="footer-high" id="white">
+            <ul class="footer-high-links">
+                <li>
+                <a href="">
+                    <p class="list-title">Stock List</p>
+                    <p class="text">在庫車両をご覧になりたい方はこちら</p>
+                </a>
+                </li>
+                <li>
+                <a href="">
+                    <p class="list-title">Feature</p>
+                    <p class="text">「バナナフィガロ」の特集サイトへ</p>
+                </a>
+                </li>
+            </ul>
+            </div>
+            <div class="footer-low">
+            <div class="footer-flex-left">
+                <img class="logo" src=${s} alt="GTBサービスのロゴ"/>
+                <address>
+                <p class="address">〒444-0951<br>岡崎市北野町字一番訳5番地1<br><span class="tel-icon"></span>0564-31-7273</p>
+                </address>
+            </div>
+            <div class="footer-flex-right">
+                <ul class="footer-low-links">
+                <li>
+                    <a href="">私たちについて</a>
+                    <ul>
+                    <li class="under-pages-links"><a href="">会社案内</a></li>
+                    <li class="under-pages-links"><a href="">アクセス</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="">事業案内</a>
+                    <ul>
+                    <li class="under-pages-links"><a href="">鈑金・塗装</a></li>
+                    <li class="under-pages-links"><a href="">整備・車検</a></li>
+                    <li class="under-pages-links"><a href="">新車・中古車販売</a></li>
+                    <li class="under-pages-links"><a href="">カスタムペイント</a></li>
+                    </ul>
+                </li>
+                <li><a href="">施工実績</a></li>
+                <li><a href="">お問い合わせ</a></li>
+                </ul>
+                <div class="instagram-link-container">
+                <p class="instagram-link"><a href="">Follow us<span class="arrow"></span></a></p>
+                </div>
+            </div>
+            </div>
+            <p class="copyright">&copy;2024 GBT service All Rights Reserved.</p>
+        </footer>
+    </div>
+`;
---
+++ assets/service-BFH2YVIj.js
@@ -0,0 +1,173 @@
+import"./ress-B710VfZE.js";import{l as i,i as l,h as e}from"./hamburger-button-CdvTyI2V.js";const a="/assets/service_main-C1SePJBZ.jpg",t="/assets/repair01-B7gXDg-X.jpg",c="/assets/repair02-Cf1Xd-se.jpg",r="/assets/repair03-DixyG-Si.jpg",n="/assets/maintenance01-BY3jhf4b.jpg",o="/assets/maintenance02-B-ZIh2Ri.jpg",p="/assets/maintenance03-BA8l2k7h.jpg",s="/assets/sale01-BYzYDplx.jpg",g="/assets/custom01-C289jzpA.jpg",v="/assets/custom02-Dro1NTFD.jpg",d="/assets/custom03-68KGQ_-A.jpg";document.querySelector("#app").innerHTML=`
+    <div>
+        <header>
+        <a href="#"><img class="logo" src=${i} alt="GTBサービスのロゴ" /></a>
+        <nav>
+            <li><a href="">私たちについて</a></li>
+            <li><a href="">事業案内</a></li>
+            <li><a href="">アクセス</a></li>
+            <li><a href="">施工実績</a></li>
+            <li class="list-contact"><a href="">お問い合わせ</a></li>
+            <li class="insta"><a href=""><img src=${l} art="Instagramアイコン"></a></li>
+        </nav>
+        <button class="hamburger">
+            <img src="${e}" alt="ハンバーアイコン" />
+        </button>
+        </header>
+        <main>
+            <div class="bgc-lightblue">
+                <section class="service">
+                    <div class="service-container">
+                        <div>
+                            <h2 class="section-title">Service</h2>
+                            <p class="section-subtitle">・事業内容・</p>
+                            <img class="service-img-sp" src=${a} alt="会社案内" />
+                            <div class="service-text">
+                                <p>自動車販売、新車から中古車やカスタムカーなど様々な種類を取り扱いしており、お客様の満足のいくお車をお探しいたします。<br>自社で鈑金塗装修理をしておりますので購入してからのアフターケアもお任せください。<br>また、鈑金、塗装、車検、修理や、今お乗りのお車を自分好みの色にカスタムしたいなど車のことなら何でもご相談してください。</p>
+                            </div>
+                        </div>
+                        <img class="service-img-pc" src=${a} alt="私たちについて" />
+                    </div>
+                </section>
+            </div>
+            <article>
+                <div class="article-container">
+                    <div class="title-flex">
+                        <h3 class="vertical-title">Repair・Paint</h3>
+                        <div class="service-content">
+                            <h4>鈑金・塗装</h4>
+                            <figure>
+                            <div class="flex-img">
+                                <img src=${t} alt="鈑金・塗装" />
+                                <div class="img-small">
+                                    <img src=${c} alt="鈑金・塗装" />
+                                    <img src=${r} alt="鈑金・塗装" />
+                                </div>
+                            </div>
+                            <figcaption>
+                                <p class="figcaption-title">経験豊富な職人が丁寧に仕上げます。</p>
+                                <p>小さな傷から大きな凹みまで、元通りにしてお返しします。<br>自動車の塗装は時間経過や紫外線によって微妙に変化していきますので、修理箇所だけではなく、車両全体に合わせた自然な色に調色をして塗装を行います。</p>
+                            </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div class="article-container">
+                    <div class="title-flex">
+                        <h3 class="vertical-title">Maintenance</h3>
+                        <div class="service-content">
+                            <h4>整備・車検</h4>
+                            <figure>
+                            <div class="flex-img">
+                                <img src=${n} alt="鈑金・塗装" />
+                                <div class="img-small">
+                                    <img src=${o} alt="鈑金・塗装" />
+                                    <img src=${p} alt="鈑金・塗装" />
+                                </div>
+                            </div>
+                            <figcaption>
+                                <p class="figcaption-title">お客様のニーズに合わせた内容でご対応します。</p>
+                                <p>お客様のお車の状態に必要な整備、必要でない整備内容を、判断させていただき、お見積りさせていただきます。作業内容の説明、料金の説明、作業日数、メンテナンスのアドバイス説明をさせていただき、お客様に納得していただいたうえで、作業にかからせていただきます。</p>
+                            </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div class="article-container">
+                    <div class="title-flex">
+                        <h3 class="vertical-title">Sale</h3>
+                        <div class="service-content">
+                            <h4>新車・中古車販売</h4>
+                            <figure>
+                            <div class="flex-img">
+                                <img src=${s} alt="鈑金・塗装" />
+                                <div class="img-small">
+                                    <img src=${s} alt="鈑金・塗装" />
+                                    <img src=${s} alt="鈑金・塗装" />
+                                </div>
+                            </div>
+                            <figcaption>
+                                <p class="figcaption-title">お客様のライフスタイルやご予算に合ったお車をご提案させていただきます。</p>
+                                <p>各メーカー新車販売、オークションによる中古車販売をしております。<br>ご購入いただいた車両の車検や修理、鈑金、レッカーなど、アフターフォローも一括でお任せいただけます。</p>
+                            </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div class="article-container">
+                    <div class="title-flex">
+                        <h3 class="vertical-title">Custom</h3>
+                        <div class="service-content">
+                            <h4>カスタムペイント</h4>
+                            <figure>
+                            <div class="flex-img">
+                                <img src=${g} alt="鈑金・塗装" />
+                                <div class="img-small">
+                                    <img src=${v} alt="鈑金・塗装" />
+                                    <img src=${d} alt="鈑金・塗装" />
+                                </div>
+                            </div>
+                            <figcaption>
+                                <p class="figcaption-title">あなただけの１台を心を込めて制作せていただきます。</p>
+                                <p>全塗装からパーツ塗装までご対応いたします。<br>また車だけでなくバイクのタンク、ヘルメットなど装飾品の塗装もお任せください。<br>お客様のご要望に合わせたカスタムをご提案いたします。</p>
+                            </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </main>
+        <footer>
+            <div class="footer-high">
+            <ul class="footer-high-links">
+                <li>
+                <a href="">
+                    <p class="list-title">Stock List</p>
+                    <p class="text">在庫車両をご覧になりたい方はこちら</p>
+                </a>
+                </li>
+                <li>
+                <a href="">
+                    <p class="list-title">Feature</p>
+                    <p class="text">「バナナフィガロ」の特集サイトへ</p>
+                </a>
+                </li>
+            </ul>
+            </div>
+            <div class="footer-low">
+            <div class="footer-flex-left">
+                <img class="logo" src=${i} alt="GTBサービスのロゴ"/>
+                <address>
+                <p class="address">〒444-0951<br>岡崎市北野町字一番訳5番地1<br><span class="tel-icon"></span>0564-31-7273</p>
+                </address>
+            </div>
+            <div class="footer-flex-right">
+                <ul class="footer-low-links">
+                <li>
+                    <a href="">私たちについて</a>
+                    <ul>
+                    <li class="under-pages-links"><a href="">会社案内</a></li>
+                    <li class="under-pages-links"><a href="">アクセス</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="">事業案内</a>
+                    <ul>
+                    <li class="under-pages-links"><a href="">鈑金・塗装</a></li>
+                    <li class="under-pages-links"><a href="">整備・車検</a></li>
+                    <li class="under-pages-links"><a href="">新車・中古車販売</a></li>
+                    <li class="under-pages-links"><a href="">カスタムペイント</a></li>
+                    </ul>
+                </li>
+                <li><a href="">施工実績</a></li>
+                <li><a href="">お問い合わせ</a></li>
+                </ul>
+                <div class="instagram-link-container">
+                <p class="instagram-link"><a href="">Follow us<span class="arrow"></span></a></p>
+                </div>
+            </div>
+            </div>
+            <p class="copyright">&copy;2024 GBT service All Rights Reserved.</p>
+        </footer>
+    </div>
+`;
