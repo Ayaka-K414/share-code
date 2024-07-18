20c0817afa27a6498f8216f9479f0138feac849c
ayakakobayashi <414ayaka@gmail.com>
2024-07-18 11:34:04 +0900

    クライアント確認前最終

---
+++ assets/works-Cv6AKzTy.js
@@ -0,0 +1,204 @@
+import"./hamburger-BO0hkOY2.js";import{l as a,i as e,h as r}from"./hamburger-button-CwQ5Ltz6.js";import{c as i}from"./close-button-D9acr4Xc.js";const s=""+new URL("archive01-C289jzpA.jpg",import.meta.url).href,t=""+new URL("archive02-Dro1NTFD.jpg",import.meta.url).href,l=""+new URL("archive03-DVSbrx4n.jpg",import.meta.url).href,c=""+new URL("archive04-Tofx9lDx.jpg",import.meta.url).href,n=""+new URL("archive05-BB3eMgHC.jpg",import.meta.url).href,o=""+new URL("archive06-BouXTYgs.jpg",import.meta.url).href,p=""+new URL("archive07-BDbSX9ib.jpg",import.meta.url).href,h=""+new URL("archive08-Cf1Xd-se.jpg",import.meta.url).href,f=""+new URL("archive09-CR5SLaTs.jpg",import.meta.url).href,g=""+new URL("archive10-FRz4teRL.jpg",import.meta.url).href,u=""+new URL("archive11-DGGVLH0a.jpg",import.meta.url).href,m=""+new URL("archive12-Ccbkycv2.jpg",import.meta.url).href;document.querySelector("#app").innerHTML=`
+    <div>
+        <header>
+            <a href="/index.html"><img class="logo" src=${a} alt="GTBサービスのロゴ" /></a>
+            <nav>
+                <li><a href="/aboutus/index.html">私たちについて</a></li>
+                <li><a href="/service/index.html">事業案内</a></li>
+                <li><a href="/aboutus/index.html#href-access">アクセス</a></li>
+                <li><a href="/works/index.html">施工実績</a></li>
+                <li class="list-contact"><a href="/aboutus/index.html#href-contact">お問い合わせ</a></li>
+                <li class="insta"><a href="https://www.instagram.com/gbtservise/" target="_blank" rel="noopener noreferrer"><img src=${e} art="Instagramアイコン"></a></li>
+            </nav>
+            <button class="hamburger-button" id="hamburger-button">
+                <img class="hamburger-icon" src="${r}" alt="ハンバーガーメニュー" />
+            </button>
+        </header>
+        <div class="menu" id="hamburger-menu">
+            <div class="text-align-right">
+                <button class="close-button" id="close-button" >
+                    <img class="close-icon" src="${i}" alt="閉じる" />
+                </button>
+            </div>
+            <div class="menu-list">
+                <ul>
+                    <li><a href="/aboutus/index.html">About us<span>私たちについて</span></a></li>
+                    <li><a href="/service/index.html">Service<span>事業案内</span></a></li>
+                    <li><a href="/works/index.html">Works<span>施工実績</span></a></li>
+                    <li class="icon"><a href="https://www.carsensor.net/shop/aichi/319305001/#contents" target="_blank" rel="noopener noreferrer">Stock List<span>在庫車両掲載サイト</span></a></li>
+                    <li class="icon"><a href="https://www.automesseweb.jp/2021/12/22/862327" target="_blank" rel="noopener noreferrer">Feature<span>当社カスタム「バナナフィガロ」特集サイト</span></a></li>
+                    <li class="icon-insta"><a href="https://www.instagram.com/gbtservise/" target="_blank" rel="noopener noreferrer">Instagram<span>インスタグラム</span></a></li>
+                </ul>
+                <a href="/index.html"><img class="logo" src=${a} alt="GTBサービスのロゴ" /></a>
+            </div>
+        </div>
+        <main>
+            <section class="works">
+                <h2 class="section-title">Works</h2>
+                <p class="section-subtitle">・施工実績・</p>
+                <div class="card-container">
+                    <figure>
+                        <a href="/post/index.html">
+                        <img src=${s} alt="鈑金・塗装" />
+                        <figcaption>
+                            <p class="card-title">カスタムペイント</p>
+                            <p class="card-text">ミラジーノのカスタムペイント</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${t} alt="整備・車検" />
+                        <figcaption>
+                            <p class="card-title">オールペン</p>
+                            <p class="card-text">ジムニーのオールペン</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${l} alt="新車・中古車販売" />
+                        <figcaption>
+                            <p class="card-title">カスタムペイント</p>
+                            <p class="card-text">グラインダーペイント</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${c} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">フルカスタム</p>
+                            <p class="card-text">ネイキッドのフルカスタム</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${n} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">オールペン</p>
+                            <p class="card-text">ミラジーノのオールペン</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${o} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">オールペン</p>
+                            <p class="card-text">ハイエースのオールペン</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${p} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">カスタムペイント</p>
+                            <p class="card-text">ベルエアのラップペイント</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${h} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">鈑金・塗装</p>
+                            <p class="card-text">アウディA7の側面キズ、凹み修理</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${f} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">カスタムペイント</p>
+                            <p class="card-text">バイクヘルメットのペイント</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${g} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">カスタムペイント</p>
+                            <p class="card-text">マスタングのカスタムペイント</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${u} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">レストア</p>
+                            <p class="card-text">ホンダN360のレストア</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                    <figure>
+                        <a href="">
+                        <img src=${m} alt="カスタムペイント" />
+                        <figcaption>
+                            <p class="card-title">カスタムペイント</p>
+                            <p class="card-text">ハーレーのカスタムペイント</p>
+                        </figcaption>
+                        </a>
+                    </figure>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <div class="footer-high">
+                <ul class="footer-high-links">
+                <li>
+                    <a href="https://www.carsensor.net/shop/aichi/319305001/#contents" target="_blank" rel="noopener noreferrer">
+                    <p class="list-title">Stock List</p>
+                    <p class="text">在庫車両をご覧になりたい方はこちら</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.automesseweb.jp/2021/12/22/862327" target="_blank" rel="noopener noreferrer">
+                    <p class="list-title">Feature</p>
+                    <p class="text">「バナナフィガロ」の特集サイトへ</p>
+                    </a>
+                </li>
+                </ul>
+            </div>
+            <div class="footer-low">
+                <div class="footer-flex-left">
+                <a href="/index.html"><img class="logo" src=${a} alt="GTBサービスのロゴ"/></a>
+                <address>
+                    <p class="address">〒444-0951<br>岡崎市北野町字一番訳5番地1<br><span class="tel-icon"></span>0564-31-7273</p>
+                </address>
+                </div>
+                <div class="footer-flex-right">
+                <ul class="footer-low-links">
+                    <li>
+                    <a href="/aboutus/index.html">私たちについて</a>
+                    <ul>
+                        <li class="under-pages-links"><a href="/aboutus/index.html">会社案内</a></li>
+                        <li class="under-pages-links"><a href="/aboutus/index.html#href-access">アクセス</a></li>
+                    </ul>
+                    </li>
+                    <li>
+                    <a href="/service/index.html">事業案内</a>
+                    <ul>
+                        <li class="under-pages-links"><a href="/service/index.html#href-repair">鈑金・塗装</a></li>
+                        <li class="under-pages-links"><a href="/service/index.html#href-maintenance">整備・車検</a></li>
+                        <li class="under-pages-links"><a href="/service/index.html#href-sale">新車・中古車販売</a></li>
+                        <li class="under-pages-links"><a href="/service/index.html#href-custom">カスタムペイント</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="/works/index.html">施工実績</a></li>
+                    <li><a href="/aboutus/index.html#href-contact">お問い合わせ</a></li>
+                </ul>
+                <div class="instagram-link-container">
+                    <p class="instagram-link"><a href="https://www.instagram.com/gbtservise/" target="_blank" rel="noopener noreferrer">Follow us<span class="arrow"></span></a></p>
+                </div>
+                </div>
+            </div>
+            <p class="copyright">&copy;2024 GBT service All Rights Reserved.</p>
+        </footer>
+    </div>
+`;
